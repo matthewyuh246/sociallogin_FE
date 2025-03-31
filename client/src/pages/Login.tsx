d62a274067a23593e1a1c7cdd5405775f381aa84
Yuhri <matthewyuh246@gmail.com>
2025-03-31 23:45:23 +0900

    pages/Login

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginForm from '../components/LoginForm'
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+      <LoginForm />
+    </div>
+  )
+}
+
+export default Login
