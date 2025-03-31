8cd449eafdd7bedd8a0e885cba84b77183216c5c
Yuhri <matthewyuh246@gmail.com>
2025-03-31 23:44:42 +0900

    pages/Dashboard

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
+      <p>Welcome to your dashboard!</p>
+    </div>
+  )
+}
+
+export default Dashboard
