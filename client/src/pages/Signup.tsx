069a5ce5d781bdf17f790e50db277bac2f29d5b4
Yuhri <matthewyuh246@gmail.com>
2025-03-31 23:45:48 +0900

    pages/Signup

---
+++ client/src/pages/Signup.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignupCallback from '../components/SignupCallback'
+
+const Signup: React.FC = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4 text-center">Signup Callback</h2>
+      <SignupCallback />
+    </div>
+  )
+}
+
+export default Signup
